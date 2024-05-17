65c92d536f62b530e8b04660c84d87085a50fec2
Mohamed Hagi <mohamed.2423@hotmail.com>
2024-05-17 00:08:34 -0400

    Changed server setup to accept lat and long

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,17 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+//Adding lat and long as parameters for the API request
 app.get("/api/weather", async (req, res) => {
-  const city = req.query.city || "Toronto";
+  const { lat, lon } = req.query;
+  if (!lat || !lon) {
+    return res
+      .status(400)
+      .json({ error: "Latitude and Longitude are required" });
+  }
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
     );
     res.json(response.data);
   } catch (error) {
