bb261329d685066b2f66926c49f6793f3b1416eb
Mohamed Hagi <mohamed.2423@hotmail.com>
2024-05-17 07:23:58 -0400

    Implemented API fetching logic

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,10 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import axios from "axios";
+import debounce from "lodash/debounce";
 
 export default function Search({ onSearchChange }) {
   const [inputValue, setInputValue] = useState("");
   const [options, setOptions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [cache, setCache] = useState({});
+
+  //memoizing the result since this can be a expensive operation, debouncing also to prevent exceed rate limit per second
+  const fetchCities = useMemo(
+    () =>
+      debounce(async (query) => {
+        if (cache[query]) {
+          setOptions(cache[query]);
+        } else {
+          setLoading(true);
+          try {
+            const response = await axios.get(`api/cities?namePrefix=${query}`);
+            const cities = response.data.data;
+            console.log(cities);
+
+            //Use a Map to remove duplicates based on city.name and city.country (there is duplicates in this api)
+            const cityMap = new Map();
+            cities.forEach((city) => {
+              const key = `${city.name}-${city.countryCode}`;
+              if (!cityMap.has(key)) {
+                cityMap.set(key, {
+                  ...city,
+                  key: `${city.name}-${city.id}`,
+                });
+              }
+            });
+
+            //cities result without duplicates
+            const uniqueCities = Array.from(cityMap.values());
+
+            setOptions(uniqueCities);
+
+            //caching cities that have already been searched to reduce requests
+            setCache((prevCache) => ({
+              ...prevCache,
+              [query]: uniqueCities,
+            }));
+          } catch (error) {
+            console.error("Error fetching cities:", error);
+          } finally {
+            setLoading(false);
+          }
+        }
+      }, 600),
+    [cache]
+  );
+
+  //fetching from api whenever the inputValue and fetchCities change
+  useEffect(() => {
+    if (inputValue) {
+      fetchCities(inputValue);
+    } else {
+      setOptions([]);
+    }
+  }, [inputValue, fetchCities]);
+
+  //when a city is selected, the inputValue shows the city and the country code, along with passing the city object to the onSearchChange,
+  //OnSearchChange will take the city that selected, and send that information to update the Weather
 
   const handleOnChange = (event, newValue) => {
     if (newValue) {
