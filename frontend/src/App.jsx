102c4172ba60760647a7da50117e0fe45b7159ec
Mohamed Hagi <mohamed.2423@hotmail.com>
2024-05-17 09:25:21 -0400

    fixed cordinates for default Toronto Location

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,11 @@ import Search from "./components/Search";
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [coordinates, setCoordinates] = useState({
-    lat: 43.65107,
-    lon: -79.347015,
+    lat: 43.7001,
+    lon: -79.4163,
   });
 
+  //fetching the current weather using the lat and lon
   useEffect(() => {
     const fetchWeather = async () => {
       try {
@@ -17,6 +18,7 @@ function App() {
           `/api/weather?lat=${coordinates.lat}&lon=${coordinates.lon}`
         );
         setWeatherData(response.data);
+        updateBackground(response.data.weather[0].icon);
       } catch (error) {
         console.error("Error fetching weather data:", error);
       }
@@ -25,6 +27,7 @@ function App() {
     fetchWeather();
   }, [coordinates]);
 
+  //setting new coordinates given by the selected city in the search, triggering a re-render and updating the weather
   const onSearchChange = (selectedCity) => {
     setCoordinates({
       lat: selectedCity.latitude,
@@ -33,8 +36,8 @@ function App() {
   };
 
   return (
-    <div>
-      <div className="flex items-center justify-start w-full bg-slate-200 h-[100vh] flex-col">
+    <div className="flex justify-center">
+      <div className="flex items-center justify-center w-80 h-[100vh] flex-col">
         <h1 className="font-bold text-3xl py-8">Weather App</h1>
         <Search onSearchChange={onSearchChange} />
         <div className="flex items-center justify-center w-80 h-80 bg-slate-200 rounded-md p-6 m-6">
